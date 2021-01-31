860acbe835b62aacd99e8c133807d55064ff4e14
shusuke0812 <shu.ota0812@gmail.com>
2021-01-31 21:41:35 +0900

    [add]sample_02: ViewModelの足算メソッドのテストを追加

--- sample_02/TestCodeSample/TestCodeSampleTests/ViewModel/MainViewModelTests.swift
+++ sample_02/TestCodeSample/TestCodeSampleTests/ViewModel/MainViewModelTests.swift
@@ -9,25 +9,21 @@ import XCTest
 @testable import TestCodeSample
 
 class MainViewModelTests: XCTestCase {
-
+    
+    private let mainViewModel = MainViewModel()
+    
+    // テスト実行前に呼ばれる（セットアップ）
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-
+    // テスト終了後に呼ばれる（後処理）
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+// MARK: - Test Method
+extension MainViewModelTests {
+    /// 足算メソッドのテスト
+    func testAdd() {
+        let result = mainViewModel.add(1, 2)
+        XCTAssertEqual(result, 3)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
