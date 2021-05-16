47634b56bc5eb24c5059e463b519281e5e6b4d4d
shusuke0812 <shu.ota0812@gmail.com>
2021-05-16 21:07:51 +0900

    [add]OSSのテストライブラリの使い方を練習するプロジェクトを追加

---
+++ UsingOssTestFramework/UsingOssTestFramework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UsingOssTestFramework
+//
+//  Created by Shusuke Ota on 2021/5/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
